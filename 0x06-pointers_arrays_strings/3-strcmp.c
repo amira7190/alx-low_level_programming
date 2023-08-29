e254032ab49c1139d8d8243c2c1ea8a5b363bd68
amira7190 <amirashehta68@gmail.com>
2023-08-29 15:49:04 -0700

    tasks

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,11 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
  * _strcmp - compare 2 strings.
  * @s1: string to be compared.
  * @s2: string for comparation.
  * Return: Difference.
- */
+*/
 int _strcmp(char *s1, char *s2)
 {
 	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
