264d11f29ba44f9a568527b527464de3b96cd9a3
amira7190 <amirashehta68@gmail.com>
2023-08-29 15:14:33 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,11 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
  * _strcat - concatenates 2 strings.
  * @dest: string with concatenation
  * @src: string to be concatenated
  * Return: Always 0.
- */
+*/
 char *_strcat(char *dest, char *src)
 {
 	int lengthD, lengthS;
