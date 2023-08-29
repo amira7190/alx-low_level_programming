0da54317497fc64edb2e08cec9bb1a1d5c878292
amira7190 <amirashehta68@gmail.com>
2023-08-29 13:50:11 -0700

    task1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers.
+ *@a: value 1 to multiplie
+ *@b: value 2 to multiplie
+ *
+ * Return: always result c
+*/
+
+int mul(int a, int b)
+{
+	int c;
+
+	/*multiplie a y b */
+	c = a * b;
+	return (c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,6 +11,7 @@ int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 void print_alphabet(void);
+int mul(int a, int b);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
