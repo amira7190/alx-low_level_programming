79efd9fb5aa371012978fdb25f1464f069a6ef07
amira7190 <amirashehta68@gmail.com>
2023-08-29 14:23:47 -0700

    task4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,6 +16,7 @@ void print_numbers(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
+void print_most_numbers(void);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
