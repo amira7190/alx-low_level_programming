054db917b49af03c2a6aaba254bf03c339a4c8c2
amira7190 <amirashehta68@gmail.com>
2023-08-31 15:59:23 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _puts_recursion - Prints a string followed by a new line
@@ -6,7 +6,7 @@
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
- */
+*/
 void _puts_recursion(char *s)
 {
 	if (*s == 0)
